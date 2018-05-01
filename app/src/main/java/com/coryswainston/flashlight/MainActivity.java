652c9adfcca30361450ed67e64d67df4b9150bf3
coryswainston <swainston.cory89@gmail.com>
2018-05-01 12:45:15 -0600

    Flashlight toggles on phone shake

--- app/src/main/java/com/coryswainston/flashlight/MainActivity.java
+++ app/src/main/java/com/coryswainston/flashlight/MainActivity.java
@@ -4,25 +4,36 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
-import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     Camera camera;
     Camera.Parameters params;
     boolean on = false;
+
+    SensorManager sensorManager;
+    SensorEvent trigger;
+    private static final int THRESHOLD = 60;
 
     private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0), SensorManager.SENSOR_DELAY_NORMAL);
 
         setContentView(R.layout.activity_main);
     }
@@ -56,6 +67,49 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        Log.d(TAG, "Event: " + sensorEvent.values[0] + "," + sensorEvent.values[1] + "," + sensorEvent.values[2]);
+
+        if (getSumAcceleration(sensorEvent) > THRESHOLD) {
+            if (trigger == null) {
+                trigger = sensorEvent;
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if (sensorEvent.timestamp - trigger.timestamp > 1000) {
+                    trigger = null;
+                } else {
+                    toggleFlashlight();
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private float getSumAcceleration(SensorEvent e) {
+        float sumAcceleration = 0;
+        for (int i = 0; i < 3; i++) {
+            sumAcceleration += Math.abs(e.values[i]);
+        }
+
+        return sumAcceleration - SensorManager.GRAVITY_EARTH;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        // ignored for now
     }
 
     private boolean getCameraPermissions() {
